276c59045f7125992cca72f5d143eca8cb1e9360
Mokwe Chibuike <mokwechibuike7@gmail.com>
2022-12-08 16:10:53 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using print f
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
