d0810eeec9308d48c91c739bee04cdba5540584f
Mokwe Chibuike <mokwechibuike7@gmail.com>
2022-12-08 16:39:05 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unstd.h>
+
+/**
+ * main - prints a line of code
+ *
+ * Return: 0 or 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
